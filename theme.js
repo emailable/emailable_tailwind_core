70a7c9d94aee844cc435ee89aac4d872e40f0d5b
Jarrett Lusso <jclusso@gmail.com>
2023-08-11 13:04:09 -0400

    Restructing as an NPM package.

--- theme.js
+++ theme.js
@@ -8,7 +8,7 @@ const spacing = [
 
 // https://uicolors.app/edit?sv1=primary:50-ecebff/100-dedbff/200-c7c2ff/300-a899ff/400-937aff/500-7e61ff/600-6a4ce1/700-5b33c7/800-3e238b/900-251452/950-11082b;slate:50-f4f4fb/100-e7e7f4/200-d9d9ed/300-c4c4e3/400-aeaed6/500-9692c4/600-8178b0/700-655b95/800-4b4276/900-332b5a/950-1d1641;blue:50-f0f8fe/100-deeefb/200-c4e3f9/300-9cd1f4/400-6db6ed/500-549ee7/600-367dda/700-2d69c8/800-2b56a2/900-274a81/950-1c2e4f;yellow:50-fff9eb/100-ffefc6/200-ffdc88/300-ffcb60/400-ffac20/500-f98907/600-dd6302/700-b74306/800-94320c/900-7a2b0d/950-461402;red:50-fff1f3/100-ffe3e6/200-ffccd4/300-ffa1b1/400-ff708a/500-f93a63/600-e7174e/700-c30d41/800-a30e3d/900-8b103b/950-4e031b
 
-export default theme = {
+export const theme = {
   borderColor: ({ theme }) => ({
     ...theme('colors'),
     DEFAULT: theme('colors.slate.100', 'currentColor'),
